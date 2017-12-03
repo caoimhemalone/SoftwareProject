3d1dd6f188e358d6f54e92fb793e822f77489ec1
Caoimhe Malone <caoimhegmalone@gmail.com>
2017-12-03 00:53:55 +0000

    More views added

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # SoftwareProject
 4th year final project 
+
+Rerences
+* https://developer.apple.com/library/content/referencelibrary/GettingStarted/DevelopiOSAppsSwift/ConnectTheUIToCode.html#//apple_ref/doc/uid/TP40015214-CH22-SW1 (November 29th)
+* https://medium.com/yay-its-erica/making-a-hamburger-slide-out-menu-in-swift-3-ef5249b6693e (december 2nd)
+* https://stackoverflow.com/questions/45139355/swift-open-new-view-after-click-on-button (December 2nd)
+* https://robots.thoughtbot.com/building-ios-interfaces-custom-button (December 2nd)
+
+
+*** Color Scheme ***
+
+Navy #0b2468
+Green #00AB44
--- SoftwareProject/ViewController.swift
+++ SoftwareProject/ViewController.swift
@@ -7,21 +7,29 @@
 //
 
 import UIKit
+import QuartzCore
 
 class ViewController: UIViewController {
     //MARK: Properties
     
+    @IBOutlet weak var onLBL: UILabel?
+
     @IBOutlet weak var loginBTN: UIButton!
+    @IBOutlet weak var registerBTN: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        onLBL?.layer.cornerRadius = 5
     }
 
 
+    //MARK: Actions
+    @IBAction func loginBTN(_ sender: UIButton) {
+        performSegue(withIdentifier: "toLoginPage", sender: self)
+    }
+    @IBAction func registerBTN(_ sender: UIButton) {
+        performSegue(withIdentifier: "toRegisterPage", sender: self)
+    }
+    
+    
 }
 
