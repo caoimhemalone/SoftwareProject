fdf4b31b28dd7e3e2053f0fffd8b09204a0fcb1c
Caoimhe(imac) <caoimhegmalone@hotmail.com>
2018-04-06 22:55:48 +0100

    Login 1 page done
    iMac

---
+++ SoftwareProjectAndroid/app/src/main/java/com/example/caoimhemalone/softwareproject/Register.java
@@ -0,0 +1,12 @@
+package com.example.caoimhemalone.softwareproject;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Register extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_content);
+    }
+}
