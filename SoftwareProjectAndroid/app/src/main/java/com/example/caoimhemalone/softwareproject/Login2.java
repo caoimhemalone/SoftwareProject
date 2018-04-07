a1e244b46ec93be6f611f2ab9260e71dfc0d4021
Caoimhe (Big Macbook Pro) <caoimhegmalone@gmail.com>
2018-04-07 19:31:52 +0100

    UI done
    MCP 2

--- SoftwareProjectAndroid/app/src/main/java/com/example/caoimhemalone/softwareproject/Login2.java
+++ SoftwareProjectAndroid/app/src/main/java/com/example/caoimhemalone/softwareproject/Login2.java
@@ -6,12 +6,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-public class Login extends AppCompatActivity {
-    private Button HBTN, RegBTN, LogBTN, LogAma;
+public class Login2 extends AppCompatActivity {
+    private Button HBTN,  LogBTN;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_content);
+        setContentView(R.layout.login2_content);
+
 
         HBTN = (Button) findViewById(R.id.homeLogBTN);
         HBTN.setOnClickListener(new View.OnClickListener() {
@@ -24,20 +25,8 @@ public class Login extends AppCompatActivity {
             }
         });
 
-
         LogBTN = (Button) findViewById(R.id.Login);
         LogBTN.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i;
-                i = new Intent(getApplicationContext(), Login2.class);
-                startActivity(i);
-                finish();
-            }
-        });
-
-        LogAma = (Button) findViewById(R.id.LoginAM);
-        LogAma.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i;
@@ -46,18 +35,5 @@ public class Login extends AppCompatActivity {
                 finish();
             }
         });
-
-        RegBTN = (Button) findViewById(R.id.SignUp);
-       RegBTN.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i;
-                i = new Intent(getApplicationContext(), Register.class);
-                startActivity(i);
-                finish();
-            }
-        });
-
-
     }
 }
--- SoftwareProjectAndroid/app/src/main/java/com/example/caoimhemalone/softwareproject/Register.java
+++ SoftwareProjectAndroid/app/src/main/java/com/example/caoimhemalone/softwareproject/Register.java
@@ -1,12 +1,28 @@
 package com.example.caoimhemalone.softwareproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class Register extends AppCompatActivity {
+    private FloatingActionButton addBTN;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_content);
+
+        addBTN = (FloatingActionButton) findViewById(R.id.addUserBTN);
+        addBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i;
+                i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
     }
 }
